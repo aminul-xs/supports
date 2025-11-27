b1f7c57118a5846a7864f73375a6cf36a93314dc
Aminul Islam <ai.xpeedstudio@gmail.com>
2025-11-27 10:01:43 +0600

    Add Author URI and formatting to header
    Added the Author URI field and improved formatting in the file header of supports.php for better metadata clarity.

--- supports.php
+++ supports.php
@@ -10,9 +10,11 @@
  * Author:            The WordPress Contributors
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Author URI:       https://wordpress.org/
  * Text Domain:       supports
  *
  * @package CreateBlock
+ * 
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
